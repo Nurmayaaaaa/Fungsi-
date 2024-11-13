43fe9c83003daff96e6356e5f8d81b437081c62b
Nurmayaaaaa <240306054.mhs@uinmataram.ac.id>
2024-11-13 18:46:06 +0800

    Create persegipanjangluaskeliling.py

---
+++ persegipanjangluaskeliling.py
@@ -0,0 +1,12 @@
+def luaspersegipanjang(panjang,lebar):
+    return panjang*lebar
+
+
+
+le = int(input("masukkan panjang :"))
+vi = int(input("masukkan lebar :"))
+
+hasil_luas = luaspersegipanjang(le,vi)
+
+print("luasnya adalah ",hasil_luas)
+
